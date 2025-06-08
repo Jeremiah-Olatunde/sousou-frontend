2bf0518c876bb9ae0415a4077bf56b891619b55f
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-06-08 11:30:11 +0100

    chore(config): tailwind

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <>
+      <h1 className="text-gray-600 font-bold">hello sousou</h1>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import { App } from "./app";
+
+const rootEl = document.getElementById("root");
+
+if (rootEl === null) {
+  throw "root element not found";
+}
+
+createRoot(rootEl).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
