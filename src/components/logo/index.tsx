f757cec52e18ec7cd7fea25749a3b9c51a3d005c
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-03 11:48:00 +0100

    feat(ui): component logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <a href="/home">
-      <span className="font-inter font-black text-3xl tracking-tight">
+      <span className="font-inter font-black text-stone-700 text-3xl tracking-tight">
         SouSou
       </span>
     </a>
